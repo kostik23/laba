bfe114ea0f9eb4b392e4194f73df296f10cbfd52
Саша <kostik23>
2022-05-13 09:52:12 +0300

    Управление содержимым

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var main = function () {
-    "use strict";
-    $(".tabs a span").toArray().forEach(function (element) {
-    //создаем обработчик щелчков для этого элемента
-        $(element).on("click", function () {
-            $(".tabs a span").removeClass("active");
-            $(element).addClass("active");
-            $("main .content").empty();
-            return false;
-        });
-    });
-};
-$(document).ready(main); 
+$("document").ready( function() {
+	$(".tabs a span").toArray().forEach(function (element) {
+		$(element).on("click", function () {
+			var $element = $(element);
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
+			$("main .content").empty();
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("Щелчок на первой вкладке!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Щелчок на второй вкладке!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Щелчок на третьей вкладке!");
+			}
+			return false;
+		});
+	});
+}); 
